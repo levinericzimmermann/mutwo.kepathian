02506aa05b9dfec3d599810126a32c8459f49e02
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-10-24 23:59:01 +0200

    converters/pitches: Support ScalePitch

--- mutwo/kepathian_converters/parameters/pitches.py
+++ mutwo/kepathian_converters/parameters/pitches.py
@@ -7,6 +7,12 @@ __all__ = ("MutwoPitchToKepathianPitch",)
 class MutwoPitchToKepathianPitch(core_converters.abc.Converter):
     def convert(self, pitch: music_parameters.abc.Pitch) -> str:
         try:
+            # WesternPitch?
             return pitch.name
         except AttributeError:
-            return int(round(pitch.frequency))
+            try:
+                # ScalePitch?
+                return str(pitch.scale_degree + 1)
+            except AttributeError:
+                # Ok, let's hope it's any pitch:
+                return str(int(round(pitch.frequency)))
